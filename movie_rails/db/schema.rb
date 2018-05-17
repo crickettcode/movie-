cda7d475293fd4210cca5b774e06be9c71b5d965
crickett <crickettcode@gmail.com>
2018-05-17 17:46:41 -0400

    seeds

--- movie_rails/db/schema.rb
+++ movie_rails/db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_17_214355) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "genre"
+    t.decimal "year"
+    t.string "synopis"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
